88d7c12718cdba9f9abd563a3a39616f4bcf4f95
MalditoBarbudo <victorgrandagarcia@gmail.com>
2023-04-13 18:10:55 +0200

    added waiter to save module

--- R/mod_saveOutput.R
+++ R/mod_saveOutput.R
@@ -115,6 +115,32 @@ mod_save <- function(
     # content function
     content = function(file) {
       
+      # we need a waiter to show the progress due to size of files when saving whole database
+      hostess_plots <- waiter::Hostess$new(infinite = TRUE)
+      hostess_plots$set_loader(
+        waiter::hostess_loader(
+          svg = 'images/hostess_image.svg',
+          progress_type = 'fill',
+          fill_direction = 'btt'
+        )
+      )
+      
+      waiter_ddbb <- waiter::Waiter$new(
+        id = 'overlay_div',
+        html = shiny::tagList(
+          hostess_plots$get_loader(),
+          shiny::h3(translate_app("progress_ddbb", lang())),
+          shiny::p(translate_app("progress_detail_plots", lang()))
+        ),
+        color = "black"
+      )
+      
+      waiter_ddbb$show()
+      hostess_plots$start()
+      on.exit(hostess_plots$close(), add = TRUE)
+      on.exit(waiter_ddbb$hide(), add = TRUE)
+      
+      
       data_columns_sel <- input$data_columns
       data_day_sel <- input$data_day
       data_format_sel <- input$data_format
