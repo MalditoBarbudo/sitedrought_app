fd957d6201e3b2e5b30ccaf353b9142c525cfa84
MalditoBarbudo <victorgrandagarcia@gmail.com>
2023-04-14 11:09:43 +0200

    updated unique_ring

--- data-raw/unique_ring.R
+++ data-raw/unique_ring.R
@@ -5,12 +5,11 @@ source('data-raw/polygon_objects_creation.R')
 source('data-raw/palette_builder.R')
 
 usethis::use_data(
-
+  # polygons needed
   all_polygons, all_parques, provincias, catalunya, parques, ordesa, aiguestortes, peri_total,
   peri_ordesa, peri_aiguestortes, ordesa_big, aiguestortes_big, parks_big,
-  palettes_dictionary,
-  language_dictionary,
-  sitedrought_var_thes,
-
+  # thesuruses
+  palettes_dictionary, language_dictionary, sitedrought_var_thes,
+  # options
   internal = TRUE, overwrite = TRUE
 )
